ffaa0fabc1a278dda30fb72cf8ec814a624655fe
duohongrui <duohongrui@duohongruideMacBook-Pro.local>
2022-08-19 22:41:56 +0800

    fix test codes with Splatter methods

--- Chunk2-Parameters Estimation and Data Simulation/Splatter_package_methods.R
+++ Chunk2-Parameters Estimation and Data Simulation/Splatter_package_methods.R
@@ -6,7 +6,12 @@
 ## data list
 data_list <- list.files("/Users/duohongrui/Desktop/preprocessed_data/")
 
-method <- c("Splat")
+method <- c("Splat",
+            "Simple",
+            "Kersplat",
+            "SplatPop",
+            "Lun",
+            "Lun2")
 
 for(i in 1:length(data_list)){
   file_name <- data_list[i]
@@ -44,27 +49,6 @@ for(i in 1:length(data_list)){
     DEA_group <- treatment
   }
   
-  ## data save file
-  save_name <- paste0(method, "_", data_id)
-  
-  ## estimation
-  message("Estimating...")
-  tryCatch({
-    estimation_result <- simpipe::estimate_parameters(
-      ref_data = counts,
-      method = method,
-      other_prior = list(group_condition = group),
-      seed = 111,
-      verbose = TRUE,
-      use_docker = FALSE)
-  }, error = function(e){
-    estimation_error_file <- paste0(save_name, "_", "estimation_error.txt")
-    sink(estimation_error_file, type = "output", split = TRUE)
-    print(e)
-    sink()
-  })
-  
-  
   #### DEGs
   if(!is.null(group)){
     message("Performing DEA...")
@@ -82,7 +66,6 @@ for(i in 1:length(data_list)){
     de.prob <- 0.1
   }
   
-  
   ### batch
   if(!is.null(batch_info)){
     batchCells <- as.numeric(table(batch_info))
@@ -93,78 +76,106 @@ for(i in 1:length(data_list)){
   ### other information
   other_prior = list(prob.group = prob.group,
                      de.prob = de.prob,
-                     batchCells = batchCells)
-  message("Simulation...")
-  tryCatch({
-    simulation_result <- simpipe::simulate_datasets(
-      parameters = estimation_result,
-      ref_data = counts,
-      other_prior = other_prior,
-      n = 1,
-      seed = 111,
-      return_format = "list",
-      verbose = TRUE,
-      use_docker = FALSE)
-  }, error = function(e){
-    simulation_error_file <- paste0(save_name, "_", "simulation_error.txt")
-    sink(simulation_error_file, type = "output", split = TRUE)
-    print(e)
-    sink()
-    next
-  })
+                     batchCells = batchCells,
+                     fc.up.group = 2,
+                     fc.down.group = 0.5,
+                     fc.group = 2,
+                     nGenes = nrow(counts))
   
-  ## estimation step monitor
-  message("Save information of simulated data...")
-  est_time <- estimation_result[[1]][["estimate_detection"]][1, 2]
-  est_peak_memory <- estimation_result[[1]][["estimate_detection"]][1, 4]
-  
-  ## simulation step monitor
-  sim_time <- simulation_result[[1]][["simulate_detection"]][1, 2]
-  sim_peak_memory <- simulation_result[[1]][["simulate_detection"]][1, 4]
-  
-  ## simulated data info
-  sim_counts <- simulation_result[[1]][["simulate_result"]][["count_data"]]
-  ## simulated cell info
-  ### group
-  sim_col_data <- simulation_result[[1]][["simulate_result"]][["col_meta"]]
-  if("group" %in% colnames(sim_col_data)){
-    group_num <- length(unique(sim_col_data$group))
-  }else{
-    group_num <- 1
+  for(method in method){
+    ## data save file
+    save_name <- paste0(method, "_", data_id)
+    message(save_name)
+    
+    ## estimation
+    message("Estimating...")
+    tryCatch({
+      estimation_result <- simpipe::estimate_parameters(
+        ref_data = counts,
+        method = method,
+        other_prior = list(group_condition = group),
+        seed = 111,
+        verbose = FALSE,
+        use_docker = FALSE)
+    }, error = function(e){
+      estimation_error_file <- paste0(save_name, "_", "estimation_error.txt")
+      sink(estimation_error_file, type = "output", split = TRUE)
+      print(e)
+      sink()
+      next
+    })
+    
+    ## simulation
+    message("Simulation...")
+    tryCatch({
+      simulation_result <- simpipe::simulate_datasets(
+        parameters = estimation_result,
+        other_prior = other_prior,
+        n = 1,
+        seed = 111,
+        return_format = "list",
+        verbose = FALSE,
+        use_docker = FALSE)
+    }, error = function(e){
+      simulation_error_file <- paste0(save_name, "_", "simulation_error.txt")
+      sink(simulation_error_file, type = "output", split = TRUE)
+      print(e)
+      sink()
+      next
+    })
+    
+    ## estimation step monitor
+    message("Save information of simulated data...")
+    est_time <- estimation_result[[1]][["estimate_detection"]][1, 2]
+    est_peak_memory <- estimation_result[[1]][["estimate_detection"]][1, 4]
+    
+    ## simulation step monitor
+    sim_time <- simulation_result[[1]][["simulate_detection"]][1, 2]
+    sim_peak_memory <- simulation_result[[1]][["simulate_detection"]][1, 4]
+    
+    ## simulated data info
+    sim_counts <- simulation_result[[1]][["simulate_result"]][["count_data"]]
+    ## simulated cell info
+    ### group
+    sim_col_data <- simulation_result[[1]][["simulate_result"]][["col_meta"]]
+    if("group" %in% colnames(sim_col_data)){
+      group_num <- length(unique(sim_col_data$group))
+    }else{
+      if("plate" %in% colnames(sim_col_data)){
+        group_num <- length(unique(sim_col_data$plate))
+      }else group_num <- 1
+    }
+    ### batch
+    if("batch" %in% colnames(sim_col_data)){
+      batch_num <- length(unique(sim_col_data$batch))
+    }else{
+      batch_num <- 1
+    }
+    ## simulated gene info
+    sim_row_data <- simulation_result[[1]][["simulate_result"]][["row_meta"]]
+    ### DEGs
+    if("de_gene" %in% colnames(sim_row_data)){
+      de_gene_num <- sum(sim_row_data$de_gene == "yes")
+    }else{
+      de_gene_num <- 0
+    }
+    sim_data_info <- list(sim_data_id = save_name,
+                          method = method,
+                          ref_data_platform = data_info$platform,
+                          cell_num = ncol(sim_counts),
+                          gene_num = nrow(sim_counts),
+                          group = group_num,
+                          batch = batch_num,
+                          de_gene_num = de_gene_num,
+                          estimate_time = est_time,
+                          estimate_memory = est_peak_memory,
+                          simulation_time = sim_time,
+                          simulation_memory = sim_peak_memory)
+    message("Save data...")
+    save_result <- list(sim_data = simulation_result[[1]][["simulate_result"]],
+                        sim_data_info = sim_data_info)
+    # saveRDS(save_result)
+    message("Done...")
+    message("-------------------------------------------------------------------")
   }
-  ### batch
-  if("batch" %in% colnames(sim_col_data)){
-    batch_num <- length(unique(sim_col_data$batch))
-  }else{
-    batch_num <- 1
-  }
-  ## simulated gene info
-  sim_row_data <- simulation_result[[1]][["simulate_result"]][["row_meta"]]
-  ### DEGs
-  if("de_gene" %in% colnames(sim_row_data)){
-    de_gene_num <- sum(sim_row_data$de_gene == "yes")
-  }else{
-    de_gene_num <- 0
-  }
-  sim_data_info <- list(sim_data_id = save_name,
-                        method = method,
-                        ref_data_platform = data_info$platform,
-                        cell_num = ncol(sim_counts),
-                        gene_num = nrow(sim_counts),
-                        group = group_num,
-                        batch = batch_num,
-                        de_gene_num = de_gene_num,
-                        estimate_time = est_time,
-                        estimate_memory = est_peak_memory,
-                        simulation_time = sim_time,
-                        simulation_memory = sim_peak_memory)
-  message("Save data...")
-  save_result <- list(sim_data = simulation_result[[1]][["simulate_result"]],
-                      sim_data_info = sim_data_info)
-  # saveRDS(save_result)
-  message("Done...")
-  message("-------------------------------------------------------------------")
 }
-
-
-
