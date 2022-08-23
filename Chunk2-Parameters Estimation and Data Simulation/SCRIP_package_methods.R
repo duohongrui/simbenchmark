f4ce781279d3a6383de6515c4c62e6cd813811f3
duohongrui <duohongrui@duohongruideMacBook-Pro.local>
2022-08-23 20:33:01 +0800

    fix codes in SCRIP package methods

---
+++ Chunk2-Parameters Estimation and Data Simulation/SCRIP_package_methods.R
@@ -0,0 +1,181 @@
+#------------------------------------------------------------------------------#
+# This file contains five simulation methods:
+# 1. SCRIP(GP-trendedBCV)  2. SCRIP(GP-commonBCV)  3. SCRIP(BGP-commonBCV)
+# 4. SCRIP(BP)  5. SCRIP(BGP-trendedBCV)
+#------------------------------------------------------------------------------#
+library(simpipe)
+library(dplyr)
+library(stringr)
+
+## data list
+data_list <- list.files("../preprocessed_data/")[1]
+
+methods <- c("GP-trendedBCV",
+             "GP-commonBCV",
+             "BGP-commonBCV",
+             "BP",
+             "BGP-trendedBCV")
+
+for(i in 1:length(data_list)){
+  file_name <- data_list[i]
+  ## read data
+  message("-------------------------------------------------------------------")
+  message(file_name)
+  message("Read data...")
+  data <- readRDS(file.path("../preprocessed_data", file_name))
+  data_info <- data$data_info
+  if(dynwrap::is_wrapper_with_expression(data$data)){
+    counts <- t(data$data[["counts"]])
+  }else{
+    counts <- data$data
+  }
+  ## data meta info
+  data_id <- data_info$id
+  ERCC <- data_info$ERCC
+  dilution <- data_info$dilution_factor
+  volumn <- data_info$volume
+  group <- data_info$group_condition
+  treatment <- data_info$treatment
+  batch_info <- data_info$batch_info
+  cluster_info <- data_info$cluster_info
+  
+  ## 1) group
+  if(is.null(group)){
+    if(!is.null(cluster_info)){
+      group <- as.numeric(as.factor(cluster_info))
+    }
+  }
+  ## 2) cluster or treatment
+  if(is.null(treatment)){
+    DEA_group <- cluster_info
+  }else{
+    DEA_group <- treatment
+  }
+  
+  #### DEGs
+  if(!is.null(group)){
+    message("Read DEA result...")
+    # result <- readRDS(paste0("../DEA_result/", data_id, ".rds"))
+    result <- simutils::perform_DEA(data = counts,
+                                    group = DEA_group,
+                                    method = "edgeRQLFDetRate")
+    de_genes_per_group <- lapply(result, function(df){
+      rownames(df)[df$PValue < 0.05]
+    })
+    de_genes <- unique(BiocGenerics::Reduce(x = de_genes_per_group, f = union))
+    prob.group <- as.numeric(table(group))/length(group)
+    de.prob <- length(de_genes)/nrow(counts)
+  }else{
+    prob.group <- 1
+    de.prob <- 0.1
+  }
+  
+  ### batch 
+  if(!is.null(batch_info)){
+    batchCells <- as.numeric(table(batch_info))
+  }else{
+    batchCells <- ncol(counts)
+  }
+  
+  ## estimation (SCRIP only uses splatEstimate function to estimate parameters from real data)
+  message("Estimating...")
+  
+  try_result <- try(
+    estimation_result <- simpipe::estimate_parameters(
+      ref_data = counts,
+      method = "SCRIP",
+      other_prior = NULL,
+      seed = 1,
+      verbose = FALSE,
+      use_docker = FALSE),
+    silent = FALSE,
+    outFile = paste0("../error_text/", save_name, "_", "estimation_error.txt"))
+  
+  if("try-error" %in% class(try_result)){
+    next
+  }
+  
+  for(method in methods){
+    ## data save file
+    save_name <- paste0("SCRIP-", method, "_", data_id)
+    message(save_name)
+    
+    ### save
+    saveRDS(estimation_result, paste0("../estimation_result/", save_name, "_estimation_result.rds"))
+    
+    ## simulation
+    message("Simulation...")
+    ### other information
+    other_prior = list(prob.group = prob.group,
+                       de.prob = de.prob,
+                       batchCells = batchCells,
+                       nGenes = nrow(counts),
+                       mode = method)
+    
+    try_result <- try(
+      simulation_result <- simpipe::simulate_datasets(
+        ref_data = counts,
+        parameters = estimation_result,
+        other_prior = other_prior,
+        n = 1,
+        seed = 1,
+        return_format = "list",
+        verbose = FALSE,
+        use_docker = FALSE),
+      silent = FALSE,
+      outFile = paste0("../error_text/", save_name, "_", "simulation_error.txt"))
+    
+    if("try-error" %in% class(try_result)){
+      next
+    }
+    
+    ## estimation step monitor
+    message("Save information of simulated data...")
+    est_time <- estimation_result[[1]][["estimate_detection"]][1, 2]
+    est_peak_memory <- estimation_result[[1]][["estimate_detection"]][1, 4]
+    
+    ## simulation step monitor
+    sim_time <- simulation_result[[1]][["simulate_detection"]][1, 2]
+    sim_peak_memory <- simulation_result[[1]][["simulate_detection"]][1, 4]
+    
+    ## simulated data info
+    sim_counts <- simulation_result[[1]][["simulate_result"]][["count_data"]]
+    ## simulated cell info
+    ### group
+    sim_col_data <- simulation_result[[1]][["simulate_result"]][["col_meta"]]
+    if("group" %in% colnames(sim_col_data)){
+      group_num <- length(unique(sim_col_data$group))
+    } else group_num <- 1
+    ### batch
+    if("batch" %in% colnames(sim_col_data)){
+      batch_num <- length(unique(sim_col_data$batch))
+    } else batch_num <- 1
+    ## simulated gene info
+    sim_row_data <- simulation_result[[1]][["simulate_result"]][["row_meta"]]
+    ### DEGs
+    if("de_gene" %in% colnames(sim_row_data)){
+      de_gene_num <- sum(sim_row_data$de_gene == "yes")
+    }else{
+      de_gene_num <- 0
+    }
+    sim_data_info <- list(sim_data_id = save_name,
+                          method = paste0("SCRIP-", method),
+                          ref_data_platform = data_info$platform,
+                          cell_num = ncol(sim_counts),
+                          gene_num = nrow(sim_counts),
+                          group = group_num,
+                          batch = batch_num,
+                          de_gene_num = de_gene_num,
+                          estimate_time = est_time,
+                          estimate_memory = est_peak_memory,
+                          simulation_time = sim_time,
+                          simulation_memory = sim_peak_memory)
+    message("Save data...")
+    save_result <- list(sim_data = simulation_result[[1]][["simulate_result"]],
+                        sim_data_info = sim_data_info)
+    ### save
+    saveRDS(save_result, paste0("../simulation_data/", save_name, ".rds"))
+    message("Done...")
+    message("-------------------------------------------------------------------")
+  }
+}
