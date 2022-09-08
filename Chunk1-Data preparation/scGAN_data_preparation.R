d5644cd8114a0c9c4850d06dbf73585164e1a9c1
duohongrui <duohongrui@cqnu.edu.cn>
2022-09-08 15:22:04 +0800

    fix scgan data preparation

--- Chunk1-Data preparation/scGAN_data_preparation.R
+++ Chunk1-Data preparation/scGAN_data_preparation.R
@@ -5,7 +5,18 @@ for(i in 1:101){
   data <- readRDS(file.path("/Users/duohongrui/Desktop/preprocessed_data", data_list[i]))
   message("-------------------------")
   message(data_list[i])
+  ## group
   data_info <- data$data_info
+  group <- data_info$group_condition
+  cluster_info <- data_info$cluster_info
+  if(is.null(group)){
+    if(!is.null(cluster_info)){
+      group <- as.numeric(as.factor(cluster_info))
+    }else{
+      next
+    }
+  }
+  group <- group-1
   data_id <- data_info$id
   if(dynwrap::is_wrapper_with_expression(data$data)){
     data <- t(data$data[["counts"]])
@@ -13,7 +24,8 @@ for(i in 1:101){
     data <- data$data
   }
   result <- simutils::scgan_data_conversion(data = data,
-                                            data_id = data_id,
+                                            data_i = data_id,
+                                            group = group,
                                             save_to_path = save_path,
                                             verbose = FALSE)
   message("Done")
