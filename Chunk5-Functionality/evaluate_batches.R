e461afe13cbee01c6c537652cb9282b2f89fcdd9
Bo Lee <libcell@cqu.edu.cn>
2023-03-15 12:41:03 +0800

    start batch eval

--- Chunk5-Functionality/evaluate_batches.R
+++ Chunk5-Functionality/evaluate_batches.R
@@ -1,12 +1,11 @@
 library(tibble)
-data_list <- list.files("F:/sim_bench/simulation_data/", pattern = "^SPARSim_")
+data_list <- list.files("F:/sim_bench/simulation_data/", pattern = "^Splat_")
 
 for(i in data_list){
   data <- readRDS(file.path("F:/sim_bench/simulation_data", i))
   if(data$sim_data_info$batch >= 2 & "batch" %in% colnames(data$sim_data$col_meta)){
     message(i)
     batch_info <- as.character(data$sim_data$col_meta$batch)
-    batch_info <- as.numeric(as.factor(batch_info))
     batch_matrics <- simutils::calculate_batch_properties(data$sim_data$count,
                                                           batch_info = batch_info,
                                                           verbose = T)
