e1d0bd886213e374acdb66256e5d3d91c57ff930
duohongrui <duohongrui@cqnu.edu.cn>
2023-03-15 09:57:30 +0800

    add batch eval

---
+++ Chunk5-Functionality/evaluate_batches.R
@@ -0,0 +1,20 @@
+library(tibble)
+data_list <- list.files("F:/sim_bench/simulation_data/", pattern = "^SPARSim_")
+
+for(i in data_list){
+  data <- readRDS(file.path("F:/sim_bench/simulation_data", i))
+  if(data$sim_data_info$batch >= 2 & "batch" %in% colnames(data$sim_data$col_meta)){
+    message(i)
+    batch_info <- as.character(data$data_info$batch_info)
+    batch_info <- as.numeric(as.factor(batch_info))
+    batch_matrics <- simutils::calculate_batch_properties(data$data$count,
+                                                          batch_info = batch_info,
+                                                          verbose = T)
+    saveRDS(batch_matrics, file.path("F:/sim_bench/batch_evaluation", i))
+    
+  }else{
+    next
+  }
+}
+
+
