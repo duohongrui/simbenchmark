e23d5716f86f4b2ae0094732c11700fce1535477
Bo Lee <libcell@cqnu.edu.cn>
2023-04-21 23:15:51 +0800

    learn SPsimSeq data property

--- Chunk3-Data properties learning/data_properties_metrics.R
+++ Chunk3-Data properties learning/data_properties_metrics.R
@@ -1,10 +1,10 @@
 library(simpipe)
-sim_data_list <- list.files("F:/sim_bench/simulation_data/", pattern = "^SplatPop")
+sim_data_list <- list.files("../simulation_data/", pattern = "^SPsimSeq")
 
 for(i in sim_data_list){
   print(i)
   message("Read simulated data...")
-  sim_result <- readRDS(file.path("F:/sim_bench/simulation_data", i))
+  sim_result <- readRDS(file.path("../simulation_data", i))
   sim_data <- as.data.frame(sim_result$sim_data$count_data)
   
   if(any(colSums(sim_data)==0)){
@@ -20,14 +20,14 @@ for(i in sim_data_list){
   sim_data_properties <- dplyr::lst(cell_properties = sim_data_cell_properties,
                                     gene_properties = sim_data_gene_properties)
   
-  saveRDS(sim_data_properties, file.path("F:/sim_bench/sim_data_properties",
+  saveRDS(sim_data_properties, file.path("../sim_data_properties",
                                          paste0(sim_result[["sim_data_info"]][["sim_data_id"]], ".rds")))
   
   ### Read real data property
   message("Read real data properties...")
   data_name <- stringr::str_extract(i, pattern = "data[0-9]+[_]")
-  ref_data_properties <- readRDS(file.path("F:/sim_bench/ref_data_properties",
-                                           list.files("F:/sim_bench/ref_data_properties/", pattern = data_name)))
+  ref_data_properties <- readRDS(file.path("../ref_data_properties",
+                                           list.files("../ref_data_properties", pattern = data_name)))
   ref_data_cell_properties <- ref_data_properties$cell_properties
   ref_data_gene_properties <- ref_data_properties$gene_properties
   
@@ -49,5 +49,5 @@ for(i in sim_data_list){
                                              ncore = 3)
   ### save result
   message("Save results...")
-  saveRDS(result, file.path("F:/sim_bench/data_properties_result", i))
+  saveRDS(result, file.path("../data_properties_result", i))
 }
