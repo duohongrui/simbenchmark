61bf647fb728552c0df7189201c2ac6bfad85bfc
Bo Lee <libcell@cqnu.edu.cn>
2023-04-22 11:01:15 +0800

    Update evaluate_batches.R
    add k for knn

--- Chunk5-Functionality/evaluate_batches.R
+++ Chunk5-Functionality/evaluate_batches.R
@@ -1,15 +1,18 @@
 library(tibble)
-data_list <- list.files("F:/sim_bench/simulation_data/", pattern = "^powsimR")
+data_list <- list.files("../simulation_data/")
 
 for(i in data_list){
-  data <- readRDS(file.path("F:/sim_bench/simulation_data", i))
+  data <- readRDS(file.path("../simulation_data", i))
   if(data$sim_data_info$batch >= 2 & "batch" %in% colnames(data$sim_data$col_meta)){
     message(i)
     batch_info <- as.character(data$sim_data$col_meta$batch)
-    batch_matrics <- simutils::calculate_batch_properties(as.matrix(data$sim_data$count),
+    k <- round(sqrt(ncol(data$sim_data$count)))
+    message(k)
+    batch_matrics <- simutils::calculate_batch_properties(data = as.matrix(data$sim_data$count),
                                                           batch_info = batch_info,
-                                                          verbose = T)
-    saveRDS(batch_matrics, file.path("F:/sim_bench/batch_evaluation", i))
+                                                          verbose = T,
+                                                          k = k)
+    saveRDS(batch_matrics, file.path("../batch_evaluation", i))
     
   }else{
     next
